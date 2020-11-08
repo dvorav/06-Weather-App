a1eab880b9784b49f8bc599b36ecda63a514050b
dvorav <d.vorav612@gmail.com>
2020-11-08 11:39:05 -0600

    added some functionality to forecast

--- assets/script.js
+++ assets/script.js
@@ -1,74 +1,108 @@
-function weatherInfo() {
-  let cityName = $("#cityValue").val()
+function currentWeatherInfo() {
+  let cityName = $("#cityValue").val();
   let apiKey = "6c5b1d4ebd6321ec8c97f9ecea056e02";
   let queryUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     cityName +
-    "&appid=" +
+    "&units=imperial&appid=" +
     apiKey;
-
+console.log(queryUrl)
   $.ajax({
     url: queryUrl,
     method: "GET",
   }).then(function (response) {
-      //Name of city 
-      let cityName = (response.name)
-      console.log(cityName)
-      $("#currentCity").text(cityName)
-    //Current temperature converted from Kelvis to Fahrenheit
-    let currentTemp = Math.round(1.8 * (response.main.temp - 273) + 32);
-    console.log(currentTemp + "F");
-    $("#currentTemp").text("Temperature: " + currentTemp + "F")
+    //Name of city
+    let cityName = response.name;
+    // console.log(cityName);
+    $("#currentCity").text(cityName);
+    //Current temperature
+    let currentTemp = Math.round(response.main.temp);
+    // console.log(currentTemp + "F");
+    $("#currentTemp").text("Temperature: " + currentTemp + "F");
 
     //Current Humidity
     let currentHumidity = response.main.humidity;
-    console.log(currentHumidity);
-    $("#currentHumid").text("Humidity: " + currentHumidity + "%")
+    // console.log(currentHumidity);
+    $("#currentHumid").text("Humidity: " + currentHumidity + "%");
 
     //Current Wind Speed  //mesasured in unit
     let currentWindSpeed = Math.floor(response.wind.speed * 2.2369);
-    console.log(currentWindSpeed + " mph");
-    $("#currentWind").text("Wind Speed: " + currentWindSpeed + "mph")
+    // console.log(currentWindSpeed + " mph");
+    $("#currentWind").text("Wind Speed: " + currentWindSpeed + "mph");
 
-//longitude and latitude
-    let long = (response.coord.lon)
-    let lat = (response.coord.lat)
-    console.log(long + "long")
-    console.log(lat + "lat")
+    //longitude and latitude
+    let long = response.coord.lon;
+    let lat = response.coord.lat;
+    
+    // console.log(long + "long");
+    // console.log(lat + "lat");
 
-   //UV Ray Index
-   urlForUV = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + long + "&appid=6c5b1d4ebd6321ec8c97f9ecea056e02"
+    //UV Ray Index
+    urlForUV =
+      "https://api.openweathermap.org/data/2.5/uvi?lat=" +
+      lat +
+      "&lon=" +
+      long +
+      "&units=imperial&appid=6c5b1d4ebd6321ec8c97f9ecea056e02";
     $.ajax({
-        url: urlForUV,
-        method: "GET",
+      url: urlForUV,
+      method: "GET",
     }).then(function (uvIndex) {
-   let currentUVIndex = Math.round(uvIndex.value) 
-        $("#currentUV").text("UV Index: " + currentUVIndex)
-})
-  });
-}
+      let currentUVIndex = Math.round(uvIndex.value);
+      $("#currentUV").text("UV Index: " + currentUVIndex);
+
+   
+    });
+
+
+    let forecastUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + long + "&exclude=current,minutely,hourly,&cnt=6&units=imperial&appid=6c5b1d4ebd6321ec8c97f9ecea056e02"
+ $.ajax({
+   url: forecastUrl,
+   method: "GET",
+ }).then( function(forecast) {
+   console.log(forecast)
+   for(let i = 0; i < 7; i++) {
+    let forecastTemp = forecast.daily[i].temp.day
+    console.log(forecastTemp + "F")
+    let forecastHumid = parseInt(forecast.daily[i].wind_speed)
+    console.log(forecastHumid + "mph")
+    
+
+
+    }
   
+ })
 
+    });
 
-$("#searchBtn").on("click", weatherInfo);
+  
+ 
 
-
-
-
-
-// function weatherInfoApi() {
-
-
-// let queryUrl = "http://api.weatherapi.com/v1/current.json?key=09d44509473c4934b0d141746200711LIVE&q=London"
-
-// $.ajax({
-//     url: queryUrl,
-//     method: "GET",
-// }).then(function (response) {
-//     console.log(response)
+}
+//i guess this one...
 
 
 
-// })}
 
-// $("#searchBtn").on("click", weatherInfoApi);
+// function forecastWeather() {
+//   let cityName2 = $("#cityValue").val();
+
+//   let apiKey = "6c5b1d4ebd6321ec8c97f9ecea056e02";
+//   let queryUrl =
+//     "http://api.openweathermap.org/data/2.5/forecast?q=" +
+//     cityName2 +
+//     "&units=imperial&appid=" +
+//     apiKey;
+
+
+//     console.log(queryUrl)
+
+
+// }
+$("#searchBtn").on("click", currentWeatherInfo);
+// $("#searchBtn").on("click", forecastWeather);
+
+
+// $("#searchBtn").on("click", function hello() {;
+// let city = $("#cityValue").val()
+// console.log(city + "bitch")  })
